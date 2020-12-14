d63b60ff36d74371cb781aeedad5720e3bea622e
a18jorcalari <a18jorcalari@inspedralbes.cat>
2020-12-14 12:25:49 +0100

    update

--- js/login_modal.js
+++ js/login_modal.js
@@ -27,6 +27,28 @@ buttonLogin.addEventListener("click", function () {
         "cards-tabs-experiences"
     );
     tabs_experiencesElement.innerHTML = `
+        <div class="d-flex">
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdown1">
+                    <button class="dropdown-item" type="button">Action</button>
+                    <button class="dropdown-item" type="button">Another action</button>
+                    <button class="dropdown-item" type="button">Something else here</button>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdown2">
+                    <button class="dropdown-item" type="button">Action</button>
+                    <button class="dropdown-item" type="button">Another action</button>
+                    <button class="dropdown-item" type="button">Something else here</button>
+                </div>
+            </div>
+        </div>
         
         <ul class="nav nav-tabs">
             <li class="nav-item">
@@ -36,6 +58,7 @@ buttonLogin.addEventListener("click", function () {
                 <a href="#allexperiences" class="nav-link" data-toggle="tab">Todas las experiencias</a>
             </li>
         </ul>
+
         <div class="tab-content">
             <div class="tab-pane fade show active" id="myexperiences">
                 <div id="myexperiences-box">
@@ -46,6 +69,7 @@ buttonLogin.addEventListener("click", function () {
                 </div>
             </div>
         </div>
+
         `;
 
     //CARDS USER EXPERIENCES
@@ -54,29 +78,6 @@ buttonLogin.addEventListener("click", function () {
     let myexperiences_boxElement = document.getElementById("myexperiences-box");
     myexperiences_boxElement.innerHTML = `
         <div class="experiencies">
-            <div class="d-flex">
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdown1">
-                        <button class="dropdown-item" type="button">Action</button>
-                        <button class="dropdown-item" type="button">Another action</button>
-                        <button class="dropdown-item" type="button">Something else here</button>
-                    </div>
-                </div>
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdown2">
-                        <button class="dropdown-item" type="button">Action</button>
-                        <button class="dropdown-item" type="button">Another action</button>
-                        <button class="dropdown-item" type="button">Something else here</button>
-                    </div>
-                </div>
-            </div>
-
             <div class="card-deck">
                 <div class="row row-cols-1 row-cols-md-3">
                     <div class="col">
@@ -84,7 +85,6 @@ buttonLogin.addEventListener("click", function () {
                             <div style="width: 100%; height: 200px; background-color: grey;"></div>
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
@@ -96,13 +96,24 @@ buttonLogin.addEventListener("click", function () {
                             <div style="width: 100%; height: 200px; background-color: grey;"></div>
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
                             </div>
                         </div>
                     </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <div style="width: 100%; height: 200px; background-color: grey;"></div>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
@@ -114,37 +125,13 @@ buttonLogin.addEventListener("click", function () {
     );
     allexperiences_boxElement.innerHTML = `
         <div class="experiencies">
-
-            <div class="d-flex">
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdown1">
-                        <button class="dropdown-item" type="button">Action</button>
-                        <button class="dropdown-item" type="button">Another action</button>
-                        <button class="dropdown-item" type="button">Something else here</button>
-                    </div>
-                </div>
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdown2">
-                        <button class="dropdown-item" type="button">Action</button>
-                        <button class="dropdown-item" type="button">Another action</button>
-                        <button class="dropdown-item" type="button">Something else here</button>
-                    </div>
-                </div>
-            </div>
             <div class="card-deck">
                 <div class="row row-cols-1 row-cols-md-3">
-                    <div div class="col">
+                    <div class="col">
                         <div class="card h-100">
                             <div style="width: 100%; height: 200px; background-color: grey;"></div>
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
@@ -156,7 +143,6 @@ buttonLogin.addEventListener("click", function () {
                             <div style="width: 100%; height: 200px; background-color: grey;"></div>
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
@@ -168,7 +154,6 @@ buttonLogin.addEventListener("click", function () {
                             <div style="width: 100%; height: 200px; background-color: grey;"></div>
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
@@ -180,7 +165,6 @@ buttonLogin.addEventListener("click", function () {
                             <div style="width: 100%; height: 200px; background-color: grey;"></div>
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
@@ -192,7 +176,6 @@ buttonLogin.addEventListener("click", function () {
                             <div style="width: 100%; height: 200px; background-color: grey;"></div>
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
@@ -204,7 +187,6 @@ buttonLogin.addEventListener("click", function () {
                             <div style="width: 100%; height: 200px; background-color: grey;"></div>
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                             </div>
                             <div class="card-footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
